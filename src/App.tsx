e504ab12cf1951a49ea2396183dedd34985a7030
Andrey Malin <andreu6454@gmail.com>
2022-07-20 15:22:49 +0300

    README.md change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
+import Technologies from "./Technologies";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+      <Header />
+      <Technologies />
+      <Footer />
     </div>
   );
 }
