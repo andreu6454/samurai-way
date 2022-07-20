265530e1409391ddd59b94629fa2f5e10cb773ed
Andrey Malin <andreu6454@gmail.com>
2022-07-20 17:05:31 +0300

    refactor to module-css

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from "./Header.module.css";
 
 const Header = () => {
     return (
-        <header className={"app-header"}>
+        <header className={style.appHeader}>
             <img src="https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn" alt={"logo"}/>
         </header>
     );
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from "./Profile.module.css";
 
 const Profile = () => {
     return (
-        <div className="app-content">
+        <div className={style.appContent}>
             <div>
                 <img height="250px" src={"https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2159024400&v=beta&t=4WV9YKR9L3PAEnppWmPPMk5xVnETtWvhZN8NexEzPwM"} alt={"content-header"}/>
             </div>
