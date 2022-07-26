152df58e73c9af4e3e3c535f12e997944e561a09
Andrey Malin <andreu6454@gmail.com>
2022-07-26 21:51:00 +0300

    Profile change

--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -4,7 +4,7 @@ import Post from "./Post/Post";
 
 const MyPosts = () => {
     return (
-        <div>
+        <div className={style.postsBlock}>
             My posts:
             <Post message={"Hello"}/>
             <Post message={"How are you?"}/>
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import style from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
         <div className={style.appContent}>
+            <ProfileInfo />
             <div>
-                <img height="200px" width="1100px" src={"https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2159024400&v=beta&t=4WV9YKR9L3PAEnppWmPPMk5xVnETtWvhZN8NexEzPwM"} alt={"content-header"}/>
+                <textarea defaultValue={"New Post"}/>
             </div>
             <div>
-                <img width="150px" src={"https://android-obzor.com/wp-content/uploads/2022/02/5-1.jpg"} alt={"avatar"}/>
+                <button>Send</button>
             </div>
-            <textarea defaultValue={"New Post"}/>
-            <button>Send</button>
+
             <MyPosts />
         </div>
     );
