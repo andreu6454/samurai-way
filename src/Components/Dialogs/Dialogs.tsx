8df091c3ce136808e156f0b126c263ff40071ddd
Andrey Malin <andreu6454@gmail.com>
2022-07-26 21:10:50 +0300

    add dialogs page

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import style from './Dialogs.module.css'
 
+
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogsItems}>
+                <div className={style.dialog + " " +style.active}>
+                    Andrey
+                </div>
+                <div className={style.dialog}>
+                    Andrey
+                </div>
+                <div className={style.dialog}>
+                    Andrey
+                </div>
+                <div className={style.dialog}>
+                    Andrey
+                </div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>hi</div>
+                <div className={style.message}>how are you?</div>
+                <div className={style.message}>YO</div>
+            </div>
         </div>
     );
 };
