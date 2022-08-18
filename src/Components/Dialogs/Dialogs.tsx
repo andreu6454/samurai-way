058cc16c63e343003e97a9dea490648ff9ae5321
Andrey Malin <andreu6454@gmail.com>
2022-08-18 22:02:41 +0300

    Add textArea and button to messages

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
 import style from './Dialogs.module.css'
 import DialogsItem from "./DialogsItem/DialogsItem";
 import MessagesItem from "./MessagesItem/MessagesItem";
-import {MessagesPageDataType} from "../../Redux/State";
+import {MessageDataType, MessagesPageDataType} from "../../Redux/State";
 
 type DialogsPropsType = {
     state: MessagesPageDataType
 }
 
 const Dialogs = (props: DialogsPropsType) => {
-
+    let count = 4;
+    const [messageData, setMessageData] = useState<Array<MessageDataType>>(props.state.MessageData)
+    const [message, setMessage] = useState<string>("")
     const Dialogs = props.state.DialogsData.map(dialog =>
         <DialogsItem
             name={dialog.name}
             id={dialog.id}
             avatar={dialog.avatar}
         />)
-    const Messages = props.state.MessageData.map(message =>
+    const Messages = messageData.map(message =>
         <MessagesItem
             message={message.message}
             id={message.id}
             userId={message.userId}
             userAvatar={message.userAvatar}
-        /> )
+        />
+    )
+
+    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.currentTarget.value)
+    }
+    const onClickAddMessage = () => {
+        const newMessage: MessageDataType = {
+            id: count,
+            userId: 2,
+            userAvatar: "https://android-obzor.com/wp-content/uploads/2022/02/9-1.jpg",
+            message: message.trim()
+        }
+        count++;
+        message.trim() !== "" && setMessageData([...messageData, newMessage])
+        setMessage("")
+    }
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+        e.key === "Enter" && onClickAddMessage()
+    }
 
     return (
         <div className={style.dialogs}>
@@ -31,7 +52,13 @@ const Dialogs = (props: DialogsPropsType) => {
             </div>
             <div className={style.messages}>
                 {Messages}
+                <div className={style.InputZone}>
+                    <textarea onChange={onChangeHandler} value={message} placeholder={"Your Message"}
+                              onKeyPress={onKeyDownHandler}></textarea>
+                    <button onClick={onClickAddMessage}> sent</button>
+                </div>
             </div>
+
         </div>
     );
 };
--- src/Components/Dialogs/MessagesItem/MessagesItem.tsx
+++ src/Components/Dialogs/MessagesItem/MessagesItem.tsx
@@ -9,9 +9,11 @@ type MessagesItemType = {
 }
 
 const MessagesItem = (props: MessagesItemType) => {
+    let tmp
+    props.userId === 1 ? tmp = style.message : tmp = style.myMessage
 
     return (
-        <div className={style.message}>
+        <div className={tmp}>
             <img src={props.userAvatar} className={style.userAvatar} alt={"avatar"}/>
             <div className={style.textBlock}>{props.message}</div>
         </div>
