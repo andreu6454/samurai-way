8cdf3b68577c9c4545f553caa838e76e7c120ef0
Andrey Malin <andreu6454@gmail.com>
2022-08-19 20:46:34 +0300

    add simple settings css

--- src/Components/Settings/Settings.tsx
+++ src/Components/Settings/Settings.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from './Settings.module.css'
 
 const Settings = () => {
     return (
-        <div>
-            Settings
+        <div className={style.settingsModule}>
+            <h1>Settings Page</h1>
         </div>
     );
 };
