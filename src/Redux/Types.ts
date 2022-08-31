77ee2f8b1c64996d529829abf40a78b802f5a2cb
Andrey Malin <andreu6454@gmail.com>
2022-08-31 22:44:51 +0300

    remove types from State.ts

---
+++ src/Redux/Types.ts
@@ -0,0 +1,54 @@
+export type StateDataType = {
+    ProfilePage: ProfilePageDataType
+    MessagesPage: MessagesPageDataType
+    NavBarPage: NavBarDataType
+    FriendsPage: FriendsPageDataType
+}
+export type NavBarDataType = {
+    NavBarData: Array<NavBarType>
+}
+export type NavBarType = {
+    id: string
+    friend: string,
+    avatar: string
+}
+export type ProfilePageDataType = {
+    PostsData: Array<PostsDataType>
+}
+export type MessagesPageDataType = {
+    DialogsData: Array<DialogsDataType>
+    MessageData: Array<MessageDataType>
+}
+export type DialogsDataType = {
+    id: number,
+    name: string,
+    avatar: string
+}
+export type MessageDataType = {
+    id: number,
+    userId: number,
+    userAvatar: string,
+    message: string
+}
+export type PostsDataType = {
+    id: string,
+    message: string,
+    likesCount: number
+}
+export type FriendsPageDataType = {
+    FriendsData: Array<FriendsType>
+    FriendsRequest: Array<FriendsRequestType>
+}
+export type FriendsType = {
+    id: string
+    friend: string,
+    surname: string,
+    avatar: string
+}
+export type FriendsRequestType = {
+    name: string,
+    surname: string,
+    id: string,
+    avatar: string,
+    mutualFriends: number
+}
