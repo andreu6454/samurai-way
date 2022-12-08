81b01338d27debf46ac0dce9c36b73211236b73c
Andrey Malin <andreu6454@gmail.com>
2022-12-08 15:58:39 +0300

    small code refactoring

--- src/Components/Settings/Settings.tsx
+++ src/Components/Settings/Settings.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import style from './Settings.module.css'
-import {store} from "../../Redux/ReduxState";
 
 const Settings = () => {
-    let reduxState = store.getState()
-    console.log(reduxState);
     return (
         <div className={style.settingsModule}>
             <h1>Settings Page</h1>
