77269b66d09a5c1287b19f050376e3300ddb1988
Andrey Malin <andreu6454@gmail.com>
2023-01-21 18:01:32 +0300

    - login запрос

--- src/Api/auth-api.ts
+++ src/Api/auth-api.ts
@@ -1,13 +1,26 @@
 import {instance} from "./users-api";
+import {LoginDataType} from "../Components/LoginPage/LoginPage";
 
 export const authApi = {
-    me(){
-        return instance.get<{data: isLoginedResponseType, resultCode: number}>('/auth/me')
+    me() {
+        return instance.get<{ data: isAuthResponseType, resultCode: number }>('/auth/me')
+    },
+    login({email, password, rememberMe}: LoginDataType) {
+        return instance.post<isLoginedResponseType>('/auth/login', {email, password, rememberMe})
     }
 }
 
-export type isLoginedResponseType = {
+export type isAuthResponseType = {
     id: number,
     email: string,
     login: string
+}
+
+export type isLoginedResponseType = {
+    resultCode: number
+    messages: string[],
+    data: {
+        userId: number
+    }
+
 }
