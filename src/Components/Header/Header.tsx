78176223fa2c899e58a977641beaec715c55c1e0
Andrey Malin <andreu6454@gmail.com>
2023-01-22 22:03:32 +0300

    - добавил logout кнопку

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import style from "./Header.module.css";
+import {Button} from "@mui/material";
+import {useDispatch, useSelector} from "react-redux";
+import {logOutTC} from "../../Redux/Reducers/authReducer";
+import {AppRootStateType} from "../../Redux/ReduxState";
 
 const Header = () => {
+    const dispatch = useDispatch()
+    const isAuth = useSelector<AppRootStateType>(state => state.auth.isAuth)
+
+    const logOutHandle = () => {
+        dispatch(logOutTC())
+    }
+
     return (
         <header className={style.appHeader}>
-            <img src="https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn" alt={"logo"}/>
+
+            <img
+                src="https://play-lh.googleusercontent.com/ahJtMe0vfOlAu1XJVQ6rcaGrQBgtrEZQefHy7SXB7jpijKhu1Kkox90XDuH8RmcBOXNn"
+                alt={"logo"}/>
+
+            {isAuth &&
+                < Button
+                sx={{mr: 2, borderRadius: "15px", bgcolor: "#0d1117"}}
+                variant="contained"
+                onClick={logOutHandle}
+                >
+                Log out
+                </Button>
+            }
         </header>
     );
 };
